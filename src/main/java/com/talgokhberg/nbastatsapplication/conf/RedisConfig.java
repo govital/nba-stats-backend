253171dda1cb9356288d58d8b8f87010dd85be0b
govital <govital3@gmail.com>
2025-03-08 13:03:42 +0200

    added redis config

---
+++ src/main/java/com/talgokhberg/nbastatsapplication/conf/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.talgokhberg.nbastatsapplication.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(@Value("${spring.data.redis.host}") String redisHost,
+                                                         @Value("${spring.data.redis.port}") int redisPort) {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // ✅ Correct - Uses JSON for object serialization
+        return template;
+    }
+}
+
+
